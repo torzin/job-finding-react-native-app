052c182a8f88bd1fec96edc05707fce04f043141
Heitor Pastorelli Silva <83820213+torzin@users.noreply.github.com>
2023-06-25 22:44:22 -0300

    Update [id].js
    removing api key

--- app/search/[id].js
+++ app/search/[id].js
@@ -26,7 +26,7 @@ const JobSearch = () => {
                 method: "GET",
                 url: `https://jsearch.p.rapidapi.com/search`,
                 headers: {
-                    "X-RapidAPI-Key": '8883235b3dmsh7515a500b345240p156e37jsnf34d850d85ba',
+                    "X-RapidAPI-Key": 'Your API KEY',
                     "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
                 },
                 params: {
